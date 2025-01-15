3f785de7f9268032f9802557f2564b2a253b302d
Lucas Damo <lucas.damo@edu.pucrs.br>
2025-01-15 02:17:56 -0300

    THE BEGGINING

--- src/main.c
+++ src/main.c
@@ -11,26 +11,31 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
+#define CANVAS_WIDHT 1280
+#define CANVAS_HEIGHT 800
+
 int main ()
 {
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(CANVAS_WIDHT, CANVAS_HEIGHT, "Hello Raylib");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
+	int x = 300;
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
 		// drawing
 		BeginDrawing();
-
+		DrawLine(x,300,x,400,BLUE);
+		DrawLine(x,500,x,600,BLUE);
+		DrawEllipse(300, 450, 20, 20, RED);
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
@@ -42,6 +47,7 @@ int main ()
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
+
 	}
 
 	// cleanup
